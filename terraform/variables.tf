d388c3f3250db506417d94376c92138c7eddc4dc
roshh4 <roshinidharmaraj@gmail.com>
2025-09-24 23:05:41 +0530

    checking deployment pt3

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,7 +7,7 @@ variable "environment" {
 variable "location" {
   description = "Azure region for resources"
   type        = string
-  default     = "East US"
+  default     = "Southeast Asia"
 }
 
 variable "db_admin_username" {
